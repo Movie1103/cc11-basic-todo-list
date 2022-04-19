7ab570f21bb8cb8d5fb727489050c0cc12856708
Movie1103 <tpawarut@gmail.com>
2022-04-19 17:11:12 +0700

    created Todolist

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
+import { v4 as uuidv4 } from "uuid";
 import Filter from "./components/filter/Filter";
 import PageLimit from "./components/page-limit/PageLimit";
 import Pagination from "./components/pagination/Pagination";
 import TodoList from "./components/todo-list/TodoList";
 import TodoInput from "./components/todo-list/TodoInput";
+import { useState } from "react";
+
+const todo = {
+  title: "Homework",
+  completed: true,
+};
+
+const initialTodoList = [
+  { title: "Homework", completed: true, id: uuidv4() },
+  { title: "Mid term exam", completed: false, id: uuidv4() },
+  { title: "Homework", completed: true, id: uuidv4() },
+];
+
+// console.log(initialTodoList);
 
 function App() {
+  const [todoList, setTodoList] = useState(initialTodoList);
+
+  const createTodo = (title) => {
+    const newTodo = { title, completed: false, id: uuidv4() };
+    const oldTodoList = [...todoList];
+    oldTodoList.unshift(newTodo);
+    setTodoList(oldTodoList);
+  };
+
   return (
     <div className="container max-w-xs pt-5">
-      <TodoInput />
+      <TodoInput createTodo={createTodo} />
       <Filter />
       <PageLimit />
-      <TodoList />
+      <TodoList todoList={todoList} />
       <Pagination />
     </div>
   );
--- src/components/todo-list/TodoInput.js
+++ src/components/todo-list/TodoInput.js
@@ -1,18 +1,27 @@
+import { useState } from "react";
 import Button from "../ui/Button";
 // <></> Fragment ~ React.Fragment
-function TodoInput() {
+function TodoInput(props) {
+  const [todoInput, setTodoInput] = useState("");
+
   return (
     <>
       <div className="input-group shadow">
-        <input type="text" className="form-control is-invalid" placeholder="Enter new todo" />
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Enter new todo"
+          value={todoInput}
+          onChange={(event) => setTodoInput(event.target.value)}
+        />
         <Button color="success">
           <i className="fa-solid fa-plus"></i>
         </Button>
-        <Button color="outline-secondary">
+        <Button color="outline-secondary" onClick={() => setTodoInput("")}>
           <i className="fa-solid fa-xmark"></i>
         </Button>
       </div>
-      <small className="text-danger">Task is required.</small>
+      {/* <small className="text-danger">Task is required.</small> */}
     </>
   );
 }
